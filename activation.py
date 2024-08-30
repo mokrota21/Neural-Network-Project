3ebceab486c0e3eeed06ca55b3d98ec8100f51e9
mokrota21 <103483579+mokrota21@users.noreply.github.com>
2024-08-30 18:47:46 +0600

    Attempt 1
    Tried to do randomly increase/decrease weights if looks like we are in local minimum

--- activation.py
+++ activation.py
@@ -1,12 +1,32 @@
-from math import exp
+from math import tanh, log, exp
 import numpy as np
 
+def tanh_prime(x):
+    return 1 - tanh(x) ** 2
+
+def softPlus(x):
+    return log(1 + exp(x))
+
+def softPlus_prime(x):
+    return 1 / (1 + exp(-1 * x))
+
+def softPlus_np(x):
+    return np.log1p(x)
+
+def softPlus_np_prime(x):
+    return 1.0 / (1 + np.exp(-1 * x))
+
 class ActivationLayer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, activation, activation_prime, vectorized=False) -> None:
+        if not vectorized:
+            activation = np.vectorize(activation)
+            activation_prime_ = np.vectorize(activation_prime)
+
+        self.activation_ = activation
+        self.activation_prime_ = activation_prime
 
     def forward(self, x):
-        return np.tanh(x)
+        return self.activation_(x)
     
     def backward(self, de, x, rate):
-        return de * (1 - np.tanh(x) ** 2)
+        return de * self.activation_prime_(x)
--- network.py
+++ network.py
@@ -1,7 +1,14 @@
+from random import randint
+import numpy as np
+
 class NeuralNetwork:
-    def __init__(self, layers, rate) -> None:
+    def __init__(self, layers, rate, threshold=0.0001) -> None:
         self.alpha_ = rate
         self.layers_ = layers
+        self.best_ = None
+        self.last_mse_ = -1
+        self.count = 0
+        self.threshold_ = threshold
 
     def forward(self, x):
         out = [x]
@@ -25,9 +32,16 @@ class NeuralNetwork:
             de = layer.backward(de, x, self.alpha_)
         return mse
     
-    def train(self, x_set, y_set):
-        assert len(x_set) == len(y_set)
-
+    def randomize_dense(self):
+        for layer in self.layers_:
+            try:
+                layer.w_ = layer.w_ * (np.random.random(layer.w_.shape) * 2 - 1)
+                layer.b_ = layer.b_ * (np.random.random(layer.b_.shape) * 2 - 1)
+            except Exception:
+                pass
+        return True
+    
+    def train_util(self, x_set, y_set):
         avg_mse = 0
 
         for i in range(len(x_set)):
@@ -35,11 +49,58 @@ class NeuralNetwork:
             x = x_set[i]
             
             outputs = self.forward(x)
-            # print(outputs)
-            # print(f"MSE: {self.backward(outputs, y_real)};{' ' * 20}Learning rate: {self.alpha_}")
-            avg_mse += self.backward(outputs, y_real)
+            mse = self.backward(outputs, y_real)
+            avg_mse += mse
+        avg_mse = avg_mse / len(x_set)
+        
+        print_best = -1 if self.best_ is None else self.best_[0]
+        print(f"New MSE: {avg_mse}, Best MSE: {print_best}")
+        if (self.best_ is None or avg_mse < self.best_[0]):
+            self.best_ = (avg_mse, self.layers_.copy())
+    
+    def too_big_alpha(self, mse):
+        return self.last_mse_ - mse < 0
+    
+    def too_small_alpha(self, mse):
+        return self.last_mse_ - mse < self.threshold_
+    
+    # When training we do random weights if local minimum achieved. 
+    def train(self, x_set, y_set):
+        assert len(x_set) == len(y_set)
+        total_mse = 0
+        count = 0
 
-        return avg_mse / len(x_set)
+        for i in range(len(x_set)):
+            y_real = y_set[i]
+            x = x_set[i]
+            
+            outputs = self.forward(x)
+            
+            mse = self.backward(outputs, y_real)
+            print(mse)
+            total_mse += mse
+            count += 1
+            if self.too_big_alpha(mse):
+                print("decrease 10 times")
+                self.alpha_ /= 10
+            elif self.too_small_alpha(mse):
+                if self.count < 10:
+                    print("increase 10 times")
+                    self.alpha_ *= 10
+                else:
+                    if (self.best_ is None or total_mse / count < self.best_[0]):
+                        self.best_ = (total_mse / count, self.layers_.copy())
+                    self.randomize_dense()
+                    self.count = 0
+                    count = 0
+                    total_mse = 0
+            
+            self.last_mse_ = mse
+
+        total_mse = total_mse / len(x_set)
+
+        self.layers_ = self.layers_ if self.best_ is None else self.best_[1]
+        return total_mse if self.best_ is None else self.best_[0]
     
     def predict(self, x):
         current = x
