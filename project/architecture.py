36c90d82286768666950a884fda3b6747d51b72f
mokrota21 <103483579+mokrota21@users.noreply.github.com>
2025-02-26 00:12:51 +0100

    Validation + Architecture modularized

--- project/architecture.py
+++ project/architecture.py
@@ -2,29 +2,32 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+device = torch.device("cuda")
 class ConvNetPooling(nn.Module):
-    def __init__(self, height, width, output):
+    def __init__(self, height, width, output, channels):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 4, 4) # 28 - 3 = 25
-        height = height - 4 + 1
-        width = width - 4 + 1
-        self.pol = nn.MaxPool2d(2) # 25 / 2 = 12
-        height = height // 2
-        width = width // 2
-        self.conv2 = nn.Conv2d(4, 10, 4) # 12 - 4 + 1 = 9
-        height = height - 4 + 1
-        width = width - 4 + 1
-        # extra poling: 19 / 2 = 9
-        height = height // 2
-        width = width // 2
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(10 * height * width, 100)
-        self.fc2 = nn.Linear(100, 10)
-        self.fc3 = nn.Linear(10, output)
+        self.conv_layers = []
+        input_size = 1 # we assume images are GS
+        kernel_size = 4 # probably doesn't matter
+        pooling_kernel = 2 # probbaly doesn't matter
+        i = 1
+        for channel_count in channels:
+            channel_count = int(channel_count)
+            conv_layer = nn.Conv2d(input_size, channel_count, kernel_size).to(device)
+            pooling_layer = nn.MaxPool2d(pooling_kernel).to(device)
+            self.conv_layers.append((conv_layer, pooling_layer))
+            height = (height - kernel_size + 1) // 2 # we assume no padding and stride=1
+            width = (width - kernel_size + 1) // 2 # we assume no padding and stride=1
+            input_size = channel_count
+        self.flatten = nn.Flatten().to(device)
+        self.fc1 = nn.Linear(channels[-1] * height * width, 100).to(device)
+        self.fc2 = nn.Linear(100, 10).to(device)
+        self.fc3 = nn.Linear(10, output).to(device)
     
     def forward(self, x):
-        x = self.pol(F.relu(self.conv1(x)))
-        x = self.pol(F.relu(self.conv2(x)))
+        for layers in self.conv_layers:
+            conv_layer, pooling_layer = layers
+            x = pooling_layer(F.relu(conv_layer(x)))
         x = self.flatten(x)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -33,16 +36,13 @@ class ConvNetPooling(nn.Module):
     
     def show_conv(self, x):
         tensors_list = []
-        x = self.conv1(x)
-        tensors_list.append(x.clone().detach())
-        x = F.relu(x)
-        tensors_list.append(x.clone().detach())
-        x = self.pol(x)
-        tensors_list.append(x.clone().detach())
-        x = self.conv2(x)
-        tensors_list.append(x.clone().detach())
-        x = F.relu(x)
-        tensors_list.append(x.clone().detach())
-        x = self.pol(x)
-        tensors_list.append(x.clone().detach())
+        for layers in self.conv_layers:
+            conv_layer = layers[0]
+            pooling_layer = layers[1]
+            x = conv_layer(x)
+            tensors_list.append(x.clone().detach())
+            x = F.relu(x)
+            tensors_list.append(x.clone().detach())
+            x = pooling_layer(x)
+            tensors_list.append(x.clone().detach())
         return tensors_list
--- project/train.py
+++ project/train.py
@@ -9,50 +9,98 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder, EMNIST
 import torchvision.transforms as transforms
+import pandas as pd
+torch.manual_seed(162179)
 
-load = True
 device = torch.device("cuda")
 batch_size = 32
+validation_counter = 20
 cur_path = os.path.abspath(__file__)
-train_path = os.path.join(dir_path, 'data', 'train')
-save_path = os.path.join(dir_path, 'model', 'cnn.pth')
+save_path = os.path.join(dir_path, 'model')
+loss_path = os.path.join(dir_path, "loss.json")
 
 to_tensor = transforms.Compose([
     transforms.Grayscale(num_output_channels=1), transforms.ToTensor()
 ])
 dataset = EMNIST(root="data_emnist", split="letters", train=True, transform=to_tensor)
+train_dataset, validation_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2])
+trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+validationloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+
 labels = [label for _, label in dataset]
 output_size = len(set(labels))
 sample_image, _ = dataset[0]
 _, width, height = list(sample_image.shape)
-trainloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-conv_network = ConvNetPooling(height=height, width=width, output=output_size).to(device)
-if load:
-    conv_network.load_state_dict(torch.load(save_path, weights_only=True))
+conv_networks_parameters = [torch.randint(1, 10, (2,)).sort(descending=False).values for _ in range(validation_counter)]
+conv_networks = []
+for parameter in conv_networks_parameters:
+    conv_networks.append(ConvNetPooling(height=height, width=width, output=output_size, channels=parameter).cuda())
+exp_powers = torch.randint(-20, -5, (validation_counter,))
+alphas = exp_powers.exp2()
 epochs = 10
-alpha = 0.001
-optimizer = torch.optim.SGD(params=conv_network.parameters(), lr=alpha)
 mse = nn.CrossEntropyLoss()
 
-for epoch in range(epochs):
+best_model = None
+best_loss = None
+best_model_no = None
+
+losses_list = []
+
+for model_no, (conv_network, alpha) in enumerate(zip(conv_networks, alphas)):
+    optimizer = torch.optim.SGD(params=conv_network.parameters(), lr=alpha)
+    loss_list = []
+    # training
+    print(f"TRAINING {model_no}")
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            image, label = data
+            label = (label - 1).to(device)
+            image = image.to(device)
+
+            optimizer.zero_grad()
+            outputs = conv_network(image).to(device)
+            # print(label)
+            # print(outputs)
+            loss = mse(outputs, label)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            loss_list.append(loss.item())
+            if i % 100 == 99:
+                print(f"Epoch {epoch + 1}, batch {i + 1}, loss {running_loss / 100:.3f}")
+                running_loss = 0.0
+    # validating
+    losses_list.append(loss_list)
+    print(f"VALIDATION {model_no}")
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        image, label = data
-        label = (label - 1).to(device)
-        image = image.to(device)
+    with torch.no_grad():
+        for i, data in enumerate(trainloader, 0):
+            image, label = data
+            label = (label - 1).to(device)
+            image = image.to(device)
 
-        optimizer.zero_grad()
-        outputs = conv_network(image).to(device)
-        # print(label)
-        # print(outputs)
-        loss = mse(outputs, label)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        if i % 100 == 99:
-            print(f"Epoch {epoch + 1}, batch {i + 1}, loss {running_loss / 100:.3f}")
-            running_loss = 0.0
+            optimizer.zero_grad()
+            outputs = conv_network(image).to(device)
+            # print(label)
+            # print(outputs)
+            loss = mse(outputs, label)
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print(f"Epoch {epoch + 1}, batch {i + 1}, total loss {running_loss:.3f}")
+        if best_loss is None or best_loss > running_loss:
+            best_loss = running_loss
+            best_model = conv_network
+            best_model_no = model_no
 
-torch.save(conv_network.state_dict(), save_path)
+print("Saving losses")
+import json
+with open(loss_path, 'a') as file:
+    file.write(json.dumps(losses_list) + '\n')
+print(f"Best model is model with channels {conv_networks_parameters[best_model_no]} and alpha rate of {alphas[best_model_no]} with loss: {best_loss}")
+for i, model in enumerate(conv_networks):
+    layers = conv_networks_parameters[i]
+    alpha = exp_powers[i]
+    torch.save(model.state_dict(), os.path.join(save_path, f"cnn_{int(alpha)}_{int(layers[0])}_{int(layers[1])}.pth"))
 
